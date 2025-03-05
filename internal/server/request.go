95c4db9f78edc960b4c708223ceeb184ff1499bc
Udo Bauer <knackwurstking.tux@gmail.com>
2025-03-05 17:02:14 +0100

    refactor: add request type

---
+++ internal/server/request.go
@@ -0,0 +1,33 @@
+package server
+
+import "github.com/knackwurstking/picow-led-server/pkg/picow"
+
+type Request struct {
+	Command Command `json:"command"`
+	// Data contains either an empty string or a json string from type `RequestData_*`
+	Data string `json:"data"`
+}
+
+type (
+	RequestData_PostApiDevice struct {
+		picow.DeviceData
+	}
+
+	RequestData_PutApiDevice struct {
+		picow.DeviceData
+	}
+
+	RequestData_DeleteApiDevice struct {
+		Addr string `json:"addr"`
+	}
+
+	RequestData_PostApiDevicePins struct {
+		Addr string     `json:"addr"`
+		Pins picow.Pins `json:"pins"`
+	}
+
+	RequestData_PostApiDeviceColor struct {
+		Addr  string      `json:"addr"`
+		Color picow.Color `json:"color"`
+	}
+)

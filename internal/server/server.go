ccdbc8d0ccc42cdc5755770900613cda2aa71258
Udo Bauer <knackwurstking.tux@gmail.com>
2025-03-05 16:10:03 +0100

    refactor: add webscoket server handler and read loop for clients

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,9 +1,14 @@
 package server
 
 import (
+	"bytes"
+	"io"
+	"log/slog"
+
+	"golang.org/x/net/websocket"
+
 	"github.com/knackwurstking/picow-led-server/pkg/event"
 	"github.com/knackwurstking/picow-led-server/pkg/picow"
-	"golang.org/x/net/websocket"
 )
 
 type Server struct {
@@ -21,5 +26,54 @@ func NewServer(a *picow.Api, e *event.Event[*picow.Api]) *Server {
 }
 
 func (s *Server) HandleWS(ws *websocket.Conn) {
-	// TODO: ...
+	defer func() {
+		s.conns.remove(ws)
+	}()
+	defer ws.Close()
+
+	// TODO: Add websocket to connections and start the main event loop, just a channel waiting for
+	// event data
+	s.conns.add(ws)
+
+	var n int
+	var err error
+	var d []byte
+	b := make([]byte, 1024)
+outer:
+	for {
+		d = make([]byte, 0)
+
+		for {
+			// ws.SetReadDeadline(time.Now().Add(time.Second * 5))
+			if n, err = ws.Read(b); err != nil || n == 0 {
+				if err == io.EOF {
+					slog.Debug("Got an end of file error")
+					break outer
+				}
+
+				if err != nil {
+					slog.Debug("Got an error", "error", err)
+				}
+
+				if n == 0 {
+					slog.Debug("Got empty data from a client")
+				}
+
+				break
+			} else {
+				slog.Debug("Add buffer to data", "size", n, "char", string(b[:n]))
+				d = append(d, b[:n]...)
+
+				if b[n-1] == '\n' {
+					slog.Debug("Detected a newline at the end")
+					break
+				}
+			}
+		}
+
+		d = bytes.TrimRight(d, "\n")
+
+		// TODO: Handle request, do somethings with data here
+		slog.Debug("Got some data from a client", "size", len(d), "data", string(d))
+	}
 }

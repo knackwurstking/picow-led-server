4429c62b339dd8043eaa2c914cd149edf1f72db7
Udo Bauer <knackwurstking.tux@gmail.com>
2025-03-05 16:21:25 +0100

    refactor: add commands

---
+++ internal/server/command.go
@@ -0,0 +1,12 @@
+package server
+
+const (
+	CommandGetApiDevices      Command = "GET api.devices"
+	CommandPostApiDevice      Command = "POST api.device"
+	CommandPutApiDevice       Command = "PUT api.device"
+	CommandDeleteApiDevice    Command = "DELETE api.device"
+	CommandPostApiDevicePins  Command = "POST api.device.pins"
+	CommandPostApiDeviceColor Command = "POST api.device.color"
+)
+
+type Command = string
